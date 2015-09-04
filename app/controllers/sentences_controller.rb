b74b88c5e2929daad2d47a0c570e68eecec8ca81
jgu <jeffwgu@gmail.com>
2015-09-03 19:13:27 -0600

    removed passing data with session

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,13 +1,15 @@
 class SentencesController < ApplicationController
   def index
     @default_sentence = '天才不过是勤奋而已。'
-    sentence = session[:sentence] || @default_sentence
+    sentence = params[:words] || @default_sentence
     numeralized_array, simplified_array, word_array, pinyin_array, def_tracking_array = WordsHelper.make_sentence(sentence)
+    englishTrans = "Patience is a virtue."
     @payload = {  charsPerPhrase: numeralized_array,
                   chinese: simplified_array,
                   wordArray: word_array,
                   pinyin: pinyin_array,
-                  defTrackingArray: def_tracking_array }.to_json
+                  defTrackingArray: def_tracking_array,
+                  englishTrans: englishTrans }.to_json
     translator = BingTranslator.new(Figaro.env.bing_id, Figaro.env.bing_secret)
     @trans_chinese = simplified_array.join
     @voiceRSS = Figaro.env.voice_rss_key
@@ -15,13 +17,8 @@ class SentencesController < ApplicationController
   end
 
   def create
-    session[:sentence] = sentence_params[:words]
-    redirect_to sentences_path
-  end
-
-  def destroy
-    session.clear
-    redirect_to sentences_path
+    sentence = sentence_params[:words]
+    redirect_to sentences_path({ words: sentence })
   end
 
   def sentence_params
